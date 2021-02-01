cf96417f429432a3aca7ac78cc1e19b537e69489
qian-qian-a <2427264412@qq.com>
2021-02-01 17:00:40 +0800

    first system

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <swiper>
+    <swiper-item>
+      <a href="">
+        <img src="~assets/img/home/swiper/swiper1.png" alt="" @load="imageLoad">
+      </a>
+    </swiper-item>
+    <swiper-item>
+      <a href="">
+        <img src="~assets/img/home/swiper/swiper2.png" alt="" @load="imageLoad">
+      </a>
+    </swiper-item>
+    <swiper-item>
+      <a href="">
+        <img src="~assets/img/home/swiper/swiper3.png" alt="" @load="imageLoad">
+      </a>
+    </swiper-item>
+    <swiper-item>
+      <a href="">
+        <img src="~assets/img/home/swiper/swiper4.png" alt="" @load="imageLoad">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import {Swiper, SwiperItem} from 'components/common/swiper'
+
+  export default {
+    name: "HomeSwiper",
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
